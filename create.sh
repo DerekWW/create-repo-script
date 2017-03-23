aff589faa30d4db64f51b4ff12f0faa8a3e1414e
DerekWW <derek.wierson@gmail.com>
2017-03-23 10:36:37 -0700

    change create.sh to match my folder structure

--- create.sh
+++ create.sh
@@ -10,11 +10,11 @@ mkdir $dirname
 cd $dirname
 
 git init
-cp /Users/wes/galvanize/scripts/create-assessment-repo/circle.yml .
-cp /Users/wes/galvanize/scripts/create-assessment-repo/.gitignore .
-cp /Users/wes/galvanize/scripts/create-assessment-repo/package.json .
-cp -r /Users/wes/galvanize/scripts/create-assessment-repo/src .
-cp -r /Users/wes/galvanize/scripts/create-assessment-repo/test .
+cp /Users/derekwierson/desktop/create-repo-script/circle.yml .
+cp /Users/derekwierson/desktop/create-repo-script/.gitignore .
+cp /Users/derekwierson/desktop/create-repo-script/package.json .
+cp -r /Users/derekwierson/desktop/create-repo-script/src .
+cp -r /Users/derekwierson/desktop/create-repo-script/test .
 sed -i "" "s/REPOSITORY_NAME/$dirname/" package.json
 echo "# $dirname" >> readme.md
 git add .
